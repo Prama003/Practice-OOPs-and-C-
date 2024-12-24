d033e8792eb71fa0f1fa9c345b831341123d2d8b
Prama003 <151642281+Prama003@users.noreply.github.com>
2024-12-24 13:20:43 +0000

    Program2

---
+++ Assignment.cpp/Program2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the no of times thank you has to be displayed"<<endl;;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cout<<"Thank you"<<endl;
+    }
+    return 0;
+}
